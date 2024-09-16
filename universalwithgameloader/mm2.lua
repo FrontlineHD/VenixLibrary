731f805697d0679cbfb52bb11a2db6f42eebf1c4
Frontline <robloxrizzler69420@gmail.com>
2024-09-16 10:24:48 +0530

    Create mm2.lua

---
+++ universalwithgameloader/mm2.lua
@@ -0,0 +1,65 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Venix | #1 Script Library", HidePremium = true, SaveConfig = true, ConfigFolder = "Venix", IntroText = "Loading Venix...", IntroIcon = "http://www.roblox.com/asset/?id=16223149136"})
+-- LUA.LIVE
+print("JNHH GAMING IS A TRUE SKID!!!")
+print("Loaded VSL")
+print("Fetching discord link...")
+print("Fetched successfully")
+print("Version B2.06E updated at 9:58AM")
+print("Checking game...")
+print("Loading universal...")
+
+local Tab1 = Window:MakeTab({
+	Name = "Home",
+	Icon = "http://www.roblox.com/asset/?id=13060262529",
+	PremiumOnly = false
+})
+local TabM2 = Window:MakeTab({
+	Name = "MM2",
+	Icon = "http://www.roblox.com/asset/?id=13060262529",
+	PremiumOnly = false
+})
+local Tab2 = Window:MakeTab({
+	Name = "Player",
+	Icon = "http://www.roblox.com/asset/?id=2795572800",
+	PremiumOnly = false
+})
+local Tab3 = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "http://www.roblox.com/asset/?id=9405930424",
+	PremiumOnly = false
+})
+local Tab4 = Window:MakeTab({
+	Name = "GUI's",
+	Icon = "http://www.roblox.com/asset/?id=16149179345",
+	PremiumOnly = false
+})
+local Tab5 = Window:MakeTab({
+	Name = "Admin",
+	Icon = "http://www.roblox.com/asset/?id=11656483170",
+	PremiumOnly = false
+})
+local Tab6 = Window:MakeTab({
+	Name = "Animations",
+	Icon = "http://www.roblox.com/asset/?id=100041384303356",
+	PremiumOnly = false
+})
+-- Notifications
+OrionLib:MakeNotification({
+	Name = "Game Detected!",
+	Content = "Loading scripts for MM2...",
+	Image = "http://www.roblox.com/asset/?id=17055169796",
+	Time = 5
+})
+OrionLib:MakeNotification({
+	Name = "Alert!",
+	Content = "Some scripts may not work for certain executors. Have fun exploiting!",
+	Image = "http://www.roblox.com/asset/?id=17055169796",
+	Time = 5
+})
+OrionLib:MakeNotification({
+	Name = "Discord",
+	Content = "Make sure to join the discord to recieve update logs!",
+	Image = "http://www.roblox.com/asset/?id=17055169796",
+	Time = 5
+})
