476cbca52988a95adfa400a4876b1d16a2371cbe
Frontline <robloxrizzler69420@gmail.com>
2024-09-16 10:25:35 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrontlineHD/VenixLibrary/main/mainscript.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FrontlineHD/VenixLibrary/main/universalwithgameloader/mm2.lua"))()
 else
   print("not mm2")
 end
