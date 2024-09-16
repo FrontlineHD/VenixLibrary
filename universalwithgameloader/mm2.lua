a48e80c8596d2b73f0f01bb341e0887dc4f3d902
Frontline <robloxrizzler69420@gmail.com>
2024-09-16 11:06:53 +0530

    Update mm2.lua

--- universalwithgameloader/mm2.lua
+++ universalwithgameloader/mm2.lua
@@ -1,6 +1,8 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Venix | #1 Script Library", HidePremium = true, SaveConfig = true, ConfigFolder = "Venix", IntroText = "Loading Venix...", IntroIcon = "http://www.roblox.com/asset/?id=16223149136"})
+
 -- LUA.LIVE
+
 print("JNHH GAMING IS A TRUE SKID!!!")
 print("Loaded VSL")
 print("Fetching discord link...")
@@ -8,6 +10,8 @@ print("Fetched successfully")
 print("Version B2.06E updated at 9:58AM")
 print("Checking game...")
 print("Loading universal..")
+
+-- Tabs
 
 local Tab1 = Window:MakeTab({
 	Name = "Home",
@@ -44,7 +48,9 @@ local Tab6 = Window:MakeTab({
 	Icon = "rbxassetid://7733955850",
 	PremiumOnly = false
 })
--- Notifications
+
+-- Noti
+
 OrionLib:MakeNotification({
 	Name = "Game Detected!",
 	Content = "Loading scripts for MM2...",
@@ -62,4 +68,19 @@ OrionLib:MakeNotification({
 	Content = "Make sure to join the discord to recieve update logs!",
 	Image = "http://www.roblox.com/asset/?id=17055169796",
 	Time = 5
+})
+-- Update Pending
+local UPDSection = Tab6:AddSection({
+	Name = "Animations"
+})
+Tab6:AddParagraph("Section Under Devlopment.","Hello there, this section is currently under development. Check back soon!")
+-- MM2 Tab
+local HL9Section = TabM2:AddSection({
+	Name = "HighlightHub"
+})
+TabM2:AddButton({
+	Name = "HighlightHub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
+  	end    
 })
