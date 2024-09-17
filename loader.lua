54cdbb6fa15f8d1164d8575374ce476f92cecc6d
Frontline <robloxrizzler69420@gmail.com>
2024-09-17 11:17:52 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,10 +2,27 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 local Player = game.Players.LocalPlayer
 local Window = OrionLib:MakeWindow({Name = "Venix Script Library | Key system.", HidePremium = true, SaveConfig = false, ConfigFolder = "KeySys09", IntroText = "Loading key system...", IntroIcon = "rbxassetid://7733965118"})
 
-_G.Key = "Hello"
+_G.Key = "koofyiskelogishreal2024"
 _G.KeyInput = "string"
 function MakeScriptHub()
   print("correct key")
+end
+
+function CorrectKey()
+	OrionLib:MakeNotification({
+	Name = "Correct Key!",
+	Content = "Loading Venix Library...",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+end
+function InCorrectKey()
+	OrionLib:MakeNotification({
+	Name = "Incorrect Key!",
+	Content = "Recheck your key, if this issue persists make a ticket in our discord.",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
 end
 
 local Tab = Window:MakeTab({
@@ -30,6 +47,9 @@ Tab:AddButton({
 	Callback = function()
       		if _G.KeyInput == _G.Key then
         MakeScriptHub()
+	CorrectKey()
+			else
+				InCorrectKey()
       end
   	end    
 })
