fa304e19b45ef3ba902714d7ab56940a7b3f625e
Frontline <robloxrizzler69420@gmail.com>
2024-09-17 11:11:04 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,35 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Venix Hub | Key system.", HidePremium = true, SaveConfig = false, ConfigFolder = "KeySys09", IntroText = "Loading key system...", IntroIcon = "rbxassetid://7733965118", Icon = "rbxassetid://7733965118"})
+local Player = game.Players.LocalPlayer
+local Window = OrionLib:MakeWindow({Name = "Venix Script Library | Key system.", HidePremium = true, SaveConfig = false, ConfigFolder = "KeySys09", IntroText = "Loading key system...", IntroIcon = "rbxassetid://7733965118"})
 
---[[
-Name = <string> - The name of the UI.
-HidePremium = <bool> - Whether or not the user details shows Premium status or not.
-SaveConfig = <bool> - Toggles the config saving in the UI.
-ConfigFolder = <string> - The name of the folder where the configs are saved.
-IntroEnabled = <bool> - Whether or not to show the intro animation.
-IntroText = <string> - Text to show in the intro animation.
-IntroIcon = <string> - URL to the image you want to use in the intro animation.
-Icon = <string> - URL to the image you want displayed on the window.
-CloseCallback = <function> - Function to execute when the window is closed.
-]]
+_G.Key = "Hello"
+_G.KeyInput = "string"
+function MakeScriptHub()
+  print("correct key")
+end
+
+local Tab = Window:MakeTab({
+	Name = "Key System",
+	Icon = "rbxassetid://7733965118",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Key"
+})
+Tab:AddTextbox({
+	Name = "Enter Key.",
+	Default = "Key goes here.",
+	TextDisappear = true,
+	Callback = function(Value)
+		_G.KeyInput = Value
+	end	  
+})
+
+Tab:AddButton({
+	Name = "Check Key",
+	Callback = function()
+      		if _G.KeyInput == _G.Key then
+        MakeScriptHub()
+      end
+  	end    
+})
